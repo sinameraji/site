878a60027f4508dfff2bec605aacced9790b2b34
sina <sina.meraji@gmail.com>
2017-12-26 21:50:19 +0800

    better styles

--- react-client/src/components/NavBar.jsx
+++ react-client/src/components/NavBar.jsx
@@ -5,9 +5,30 @@ class NavBar extends Component {
     render() {
       return (
         <header>
-          <ul id="headerButtons">
-            <li className="navButton"><Link to="">Home</Link></li>
-          </ul>
+          <h1>SinaLab</h1>
+        
+          <nav>
+            <input type="checkbox" id="nav" class="hidden"/>
+            <label for="nav" class="nav-open"><i></i><i></i><i></i></label>
+            <div class="nav-container">
+              <ul>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+            
+            
+            
+{/*             
+            <ul id="headerButtons">
+              <li className="navButton"><Link to="">Home</Link></li>
+              <li className="navButton"><Link to="">Book collection</Link></li>
+              <li className="navButton"><Link to="">Github</Link></li>
+              <li className="navButton"><Link to="">Blog</Link></li>
+            </ul> */}
         </header>
       )
     }
