7ebccdf029da0804c760ec27e7f271d03fafa558
sina <sina.meraji@gmail.com>
2017-12-29 01:51:00 +0800

    add all side projects

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react'
-
+import SideProjects from './SideProjects'
 class Header extends Component {
     render() {
       return (
         <div>
-          {/* <p>Hi! I'm Sina Meraji, a 3rd year artificial intelligence undergrad at University of Malaya. I'm from Iran, but I've been living in Malaysia for the past 8 years. 
-            I have a <i>side passion</i> for building little blocks of human lifestyle and test them in my communities. 
-          </p> */}
           <h2>Side projects</h2>
-          <ul class="solution">
-            <li>App Club</li>
-            <li>CrossFac</li>
-            <li>CrossUni</li>
-            <li>AI Newsletter (work in progress)</li>
-          </ul>
+          <SideProjects />
         </div>
       )
     }
--- react-client/src/components/SideProjects.jsx
+++ react-client/src/components/SideProjects.jsx
@@ -8,8 +8,8 @@ export default class SideProjects extends Component {
             <ul class="cards">
                 
                 <SideProject name="App Club" type="Community" desc="A 2-year program to build the tech startup ecosystem in UM with friends, by rethinking the way we learn technology. We're making it work." img={require('../images/appclub.jpg')} />
-                <SideProject name="CrossUni" type="Software/Experience" desc="A side project with 2 great friends in Johor. CrossUni connects students form 5 universities in Johor to each other, based on their interests." img={require('../images/appclub.jpg')} />
-                <SideProject name="CrossFac" type="Software/Experience" desc="Made this first, before CrossUni, to connect like-minded UM students from different faculties. Decided to pause it temporarily and execute CrossUni in Johor first, for good reasons." img={require('../images/appclub.jpg')} />
+                <SideProject name="CrossUni" type="Software/Experience" desc="A side project with 2 great friends in Johor. CrossUni connects students form 5 universities in Johor to each other, based on their interests." img={require('../images/crossuni.jpg')} />
+                <SideProject name="CrossFac" type="Software/Experience" desc="Made this first, before CrossUni, to connect like-minded UM students from different faculties. Decided to pause it temporarily and execute CrossUni in Johor first, for good reasons." img={require('../images/crossfac.jpg')} />
               
                 
                 
