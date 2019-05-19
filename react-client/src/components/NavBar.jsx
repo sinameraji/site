17be7a1dfc6c9b78c862442d641e39de774293b5
Sina Meraji <sina.meraji@gmail.com>
2019-05-19 15:52:43 +0800

    trying routing to separate bookshelf from homepage

--- react-client/src/components/NavBar.jsx
+++ react-client/src/components/NavBar.jsx
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Books from './Books';
 
 class NavBar extends Component {
     render() {
       return (
-        <header>
-          <div class="harambe">
-            <img src={ require('../images/sina.jpg') } height="20em"/>
-          </div>
-          <a id ="sina" href="/"><h1>Sina Meraji</h1></a>
-          <div class="card-container"><h4>Technical generalist, AI Product Manager at Supahands.com <br/> I have a <i>side passion</i> for building, testing and optimising little blocks of human lifestyle.</h4></div>
-          <nav>
-            <input type="checkbox" id="nav" class="hidden"/>
-            <label for="nav" class="nav-open"><i></i></label>
-            <div class="nav-container">
-              <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="https://www.facebook.com/AppClubUM/">Library</a></li>
-                <li><a href="http://medium.com/@sinameraji">Blog</a></li>
-                <li><a href="http://medium.com/@sinameraji">Side projects</a></li>
-                <li><a href="https://github.com/sinameraji/">GitHub</a></li>
-                {/* <li><a href="https://www.facebook.com/AppClubUM/">App Club😍</a></li> */}
-              </ul>
+        <Router>
+          <header>
+            <div class="harambe">
+              <img src={ require('../images/sina.jpg') } height="20em"/>
             </div>
-          </nav>
-        </header>
+            <a id ="sina" href="/"><h1>Sina Meraji</h1></a>
+            <div class="card-container"><h4>Technical generalist, AI Product Manager at Supahands.com <br/> I have a <i>side passion</i> for building, testing and optimising little blocks of human lifestyle.</h4></div>
+            <nav>
+              <input type="checkbox" id="nav" class="hidden"/>
+              <label for="nav" class="nav-open"><i></i></label>
+              <div class="nav-container">
+                <ul>
+                  <li><a href="/">Home</a></li>
+                  <li><Link to="/library">Library</Link></li>
+                  <li><a href="http://medium.com/@sinameraji">Blog</a></li>
+                  <li><a href="#">Side projects</a></li>
+                  <li><a href="https://github.com/sinameraji/">GitHub</a></li>
+                  {/* <li><a href="https://www.facebook.com/AppClubUM/">App Club😍</a></li> */}
+                </ul>
+                <Route path="/library/" component={Books} />
+              </div>
+            </nav>
+          </header>
+        </Router>
       )
     }
   }
