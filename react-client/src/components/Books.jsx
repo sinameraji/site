028a0276e9fb2d372ab7f68f072b252e30ea6438
Sina Meraji <sina.meraji@gmail.com>
2019-05-19 15:09:55 +0800

    comment out the part that fetched books from a json

--- react-client/src/components/Books.jsx
+++ react-client/src/components/Books.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Book from './Book'
-import bookData from '../data/books.json'
+// import bookData from '../data/books.json'
 export default class Books extends Component {
     
     render(){
@@ -12,7 +12,7 @@ export default class Books extends Component {
         return (
             <ul class="cards">
                 {books.map(b => <Book title={b.title} author={b.author} desc={b.desc} img={(b.img)} />)}
-                {/* <Book title="The Leadership Pipeline" author="Ram Charan" desc="Reading currently. I look forward to learn how to grow as a leader, while growing other leaders in the system. Building a solid leadership pipeline is crucial for teams, businesses and civilizations." img={require('../images/leadership-pipeline.jpg')} />
+                <Book title="The Leadership Pipeline" author="Ram Charan" desc="Reading currently. I look forward to learn how to grow as a leader, while growing other leaders in the system. Building a solid leadership pipeline is crucial for teams, businesses and civilizations." img={require('../images/leadership-pipeline.jpg')} />
                 <Book title="Zero to One" author="Peter Thiel" desc="It aligned with my thoughts about the concept of competition in individual and social context, labeled people like me as Definite Optimists! It was so good I read about 40 pages a day despite my hectic schedule." img={require('../images/zero-to-one.jpg')} />
                 <Book title="Solving Tough Problems" author="Adam Kahane" desc="Loved this book. A great read on transitioning from solving science problems to solving real world problems. Gave me the courage to be more open about my pursuit of leadership and tackling the education problem." img={require('../images/solving-tough-problems.jpg')} />
                 <Book title="Misbehaving" author="Richard Thaler" desc="This year (2018) I found a deep interest in 2 things: Economics and understanding/influencing human behavior. This book was quite interesting, I read half of it." img={require('../images/misbehaving.jpg')} />
@@ -26,7 +26,7 @@ export default class Books extends Component {
                 <Book title="Ali's Attraction and Repulsion" author="Morteza Motahhari" desc="A brilliant low profile leadership book. Defines justice, hypocrisy and leadership by addressing leadership's side effect of making friends and enemies." img={require('../images/ali.jpg')} />
                 <Book title="The matter of bank and riba (usury)" author="Morteza Motahhari" desc="Transcript of an extraordinary socioeconomic argument on Islam's view on banking." img={require('../images/bank.jpg')} />
                 <Book title="Computer Organization And Architecture" author="William Stallings" desc="Found a deep interest in computer hardware in my first year at uni. The idea of improving PC performance by fundamentally changing parts of the architecture is fascinating." img={require('../images/csa.jpg')} />
-                <Book title="Thinking, fast and slow" author="Daniel Kahneman" desc="I liked it because it articulated my thoughts, and stopped reading it for the same reason." img={require('../images/thinking.jpg')} /> */}
+                <Book title="Thinking, fast and slow" author="Daniel Kahneman" desc="I liked it because it articulated my thoughts, and stopped reading it for the same reason." img={require('../images/thinking.jpg')} />
                 
                 
             </ul>
